191a790009a0e46ec1410a65bc07389c10a47b6c
CoCoSol007 <solois.corentin@gmail.com>
2024-06-01 23:04:31 +0200

    Add the code related to the pause menu - closes: #16

--- scripts/pause/pause.gd
+++ scripts/pause/pause.gd
@@ -1,11 +1,26 @@
-extends Node
+"""
+The code related to the pause in the game.
+"""
 
+extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var beat_manager: BeatManager = $"../BeatManager"
+var paused: bool = false;
 
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"): pause()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func pause():
+	if paused:
+		for child in get_children(): child.hide()
+		Engine.time_scale = 1
+		beat_manager.paused = false
+	else:
+		for child in get_children(): child.show()
+		Engine.time_scale = 0
+		beat_manager.paused = true
+	paused = !paused
+
+func _on_quit_pressed():
+	SceneTransition.change_scene("res://scenes/menu/menu.tscn")
+	Engine.time_scale = 1
