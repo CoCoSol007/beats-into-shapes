887218ed0b4c9245bf8c38bf9abe7cbcf8389a58
CoCoSol007 <solois.corentin@gmail.com>
2024-06-01 22:46:26 +0200

    Add transition programs - closes: #13

--- scripts/scene_transition/scene_transition.gd
+++ scripts/scene_transition/scene_transition.gd
@@ -1,11 +1,15 @@
-extends Node
+"""
+The script to launch smooth scene transitions.
+"""
 
+extends CanvasLayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func change_scene(target: String):
+	# play the animation.
+	$Animation.play("hit")
+	await $Animation.animation_finished
 
+	# Change the scene.
+	get_tree().change_scene_to_file(target)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	$Animation.play("remove")
