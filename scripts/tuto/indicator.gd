2d333e93b216729be9a11d188ff8cf97eda933f1
CoCoSol007 <solois.corentin@gmail.com>
2024-06-03 18:22:29 +0200

    WIP: Imrpove code fmt

--- scripts/tuto/indicator.gd
+++ scripts/tuto/indicator.gd
@@ -9,17 +9,30 @@ class_name Indicator extends Sprite2D
 
 var bonus_line_length: float = 0.0
 
+# The current state of the indicator.
 var pressed:
 	get: return null
+	# Sets the state of the indicator to pressed or unpressed.
 	set(value):
+		# If the indicator is pressed, use the pressed texture,
+		# otherwise use the unpressed texture.
 		if value: texture = pressed_texture
 		else: texture = unpressed_texture
 
+# The remaining time of the indicator.
 var remaining_time:
 	get: return null
+	# Sets the remaining time of the indicator.
 	set(value):
+		# Updates the first line of the indicator.
 		$Line2D.set_point_position(1, direction * max(0.0, value) * line_scale + line_offset)
+		# Updates the second line of the indicator.
 		$Line2D2.set_point_position(1, direction * max(0.0, value + bonus_line_length) * line_scale + line_offset)
+		
+		# If the remaining time is negative, the bonus line is added to the remaining time.
+		# If the remaining time is zero, the label is set to an empty string.
+		# Otherwise, the label is set to the remaining time rounded to one decimal place.
 		if value < 0.0: value = max(0.0, bonus_line_length + value)
 		if value == 0.0: $Label.text = ""
 		else: $Label.text = "%.1f" % value
+
