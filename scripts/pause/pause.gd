6a24dd1e2fc3e55e3385cfa7cc42fd388774b6fe
Yannis300307 <yannis300307@gmail.com>
2024-10-24 19:20:59 +0200

    Prevent game lock on pause

--- scripts/pause/pause.gd
+++ scripts/pause/pause.gd
@@ -13,21 +13,22 @@ func _process(_delta):
 	if Input.is_action_just_pressed("pause"): pause()
 
 func pause():
-	paused = !paused
-	if paused:
-		for child in get_children(): 
-			if child.name != "Animation":
-				child.show()
-		$Animation.play("Spawn")
-	else:
-		$Animation.play("Despawn")
-		await $Animation.animation_finished
-		for child in get_children(): 
-			if child.name != "Animation":
-				child.hide()
-	for child in pause_affected_script.get_children():
-		if child.paused != null: 
-			child.paused = paused
+	if not $Animation.is_playing():
+		paused = !paused
+		if paused:
+			for child in get_children(): 
+				if child.name != "Animation":
+					child.show()
+			$Animation.play("Spawn")
+		else:
+			$Animation.play("Despawn")
+			await $Animation.animation_finished
+			for child in get_children(): 
+				if child.name != "Animation":
+					child.hide()
+		for child in pause_affected_script.get_children():
+			if child.paused != null: 
+				child.paused = paused
  
 func _on_quit_pressed():
 	SceneTransition.change_scene("res://scenes/menu/menu.tscn")
